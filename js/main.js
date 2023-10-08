750bba7888316379371ba5749ebe7e9ef114e817
Mich Aguilar <144078261+aracelymichel@users.noreply.github.com>
2023-10-08 11:11:28 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,84 @@
+/* El código `$(function(){...});` es una abreviatura de `$(document).ready(function(){...});`. Se
+utiliza para ejecutar el código dentro de la función una vez que el DOM (Modelo de objetos de
+documento) está listo. */
+;
+$(function(){
+    $('a[href*=#]').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+        && location.hostname == this.hostname) {
+            var $target = $(this.hash);
+            $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+            if ($target.length) {
+                var targetOffset = $target.offset().top;
+                $('html,body').animate({scrollTop: targetOffset}, 800);
+                return false;
+            }
+        }
+    });
+});
+/* El código `var TxtType = function(el, toRotate, period) {...}` define una función constructora
+llamada `TxtType`. Esta función constructora se utiliza para crear objetos que representan un efecto
+de máquina de escribir en una página web. */
+var TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+/* La función `TxtType.prototype.tick` es responsable del efecto de escritura en la página web. Se
+llama repetidamente para actualizar el texto que se muestra. */
+TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+    }
+
+    setTimeout(function() {
+        that.tick();
+    }, delta);
+};
+
+/* El código `window.onload = function() {...}` es un controlador de eventos que se activa cuando la
+ventana ha terminado de cargarse. Se utiliza para ejecutar el código dentro de la función una vez
+que se ha cargado todo el contenido HTML, incluidas imágenes y scripts. */
+window.onload = function() {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i = 0; i < elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+            new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+    document.body.appendChild(css);
+}
+
+
